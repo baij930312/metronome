233373fd848fb7632b5ee2cb38e50c7dd01c75d0
baijin <2368276566@qq.com>
2018-12-22 22:14:43 +0800

    add : play state model

--- lib/screen/home/model/play_state.dart
+++ lib/screen/home/model/play_state.dart
@@ -1,10 +1,17 @@
-class PlayState{
-  int index;//节奏item索引
-  bool playing;//播放状态
-  int totelCount;//总小节数
-  int currentCount;//当前小结数
-  int totelCountOfBar;//每小节拍数
-  int currentBeat;//在小节中时第几拍
+class PlayState {
+  int index; //节奏item索引
+  bool playing; //播放状态
+  int currentBarIndex; //当前小结数
+  int totelCountOfBar; //总小节数
+  int currentBeatIndex; //在小节中时第几拍
+  int totelBeatsOfBar; //每小节拍数
 
-  PlayState({this.index,this.playing,this.totelCount,this.currentCount});
+  PlayState({
+    this.index,
+    this.playing,
+    this.totelCountOfBar,
+    this.currentBarIndex,
+    this.totelBeatsOfBar,
+    this.currentBeatIndex,
+  });
 }
